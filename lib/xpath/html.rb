94c46fb3a2b3f7761e4f7c379a469986cb1a6bc9
Piotr Jakubowski <piotrj@gmail.com>
2010-12-26 17:40:07 +0100

    Added missing locate_field removed in 9809f35c7f8d3fe156bf

--- lib/xpath/html.rb
+++ lib/xpath/html.rb
@@ -38,6 +38,7 @@ module XPath
 
     def fillable_field(locator, options={})
       xpath = descendant(:input, :textarea)[~attr(:type).one_of('submit', 'image', 'radio', 'checkbox', 'hidden', 'file')]
+      xpath = locate_field(xpath, locator)
       xpath = xpath[field_value(options[:with])] if options.has_key?(:with)
       xpath
     end
